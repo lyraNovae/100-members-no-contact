c52db19bad919f2ead7da646ff63f5d916b9bc16
CheeseB0y <cheeseb0y1316@gmail.com>
2026-06-20 14:12:09 -0400

    Player now loses a small amount of spin veloicty per collision

--- Actual Game Folder/scripts/player.gd
+++ Actual Game Folder/scripts/player.gd
@@ -7,21 +7,32 @@ extends RigidBody2D
 # What's the mechanic like? I'm not sure about the core mechanic right now
 # Is player going to control the beyblade? Against who? It could be speedrun type game.
 
-@export var starting_spin_velocity:float = 30;
+# export these values to make it easier to adjust
+# we can tweek these until the game feels fun
+@export var starting_spin_velocity:float = 30
+@export var default_velocity: float = 20
+@export var spin_velocity_drop_on_collision: float = 1
+@export var spin_velocity_drop_over_time: float = 1
 
-
-var default_velocity: float = 20;
+var current_velocity: Vector2 = Vector2(0, 0)
 var spin_velocity: float = starting_spin_velocity
 var player_died: bool = false
 
+func _ready() -> void:
+	# this is necessary for _on_body_entered, 1 is technically enough for just the player but with multiple bayblades we might need to increase this value.
+	max_contacts_reported = 5
+
 func _physics_process(delta: float) -> void:
+
 	$Sprite2D.rotate(spin_velocity * delta)
+
 	if spin_velocity > 0:
-		spin_velocity -= delta
+		spin_velocity -= spin_velocity_drop_over_time * delta
 	else:
 		player_died = true
+		spin_velocity = 0 # prevents slight backspin on player death
 	
-	var current_velocity = Vector2(0, 0);
+	current_velocity = Vector2(0, 0);
 
 	if Input.is_action_pressed("left"):
 		current_velocity[0] -= default_velocity;
@@ -37,4 +48,10 @@ func _physics_process(delta: float) -> void:
 
 	apply_force(current_velocity * spin_velocity) #Just proprtional to spin velocity rn, some physics guy please make cleaner logic idk how beyblades work
 
+	pass
+
+# slightly lower spin velocity every time there is a collision with another rigid body
+# we can add ways to increase your spin later to give the player more control
+func _on_body_entered(body: Node) -> void:
+	spin_velocity -= spin_velocity_drop_on_collision
 	pass
